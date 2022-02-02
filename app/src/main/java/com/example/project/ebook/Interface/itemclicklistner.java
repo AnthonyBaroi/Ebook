39c33c5be59fc26adc4643719a8b18296010b2df
AnthonyBaroi <69390725+AnthonyBaroi@users.noreply.github.com>
2022-02-02 23:38:30 +0600

    Initial commit

---
+++ app/src/main/java/com/example/project/ebook/Interface/itemclicklistner.java
@@ -0,0 +1,8 @@
+package com.example.project.ebook.Interface;
+
+import android.view.View;
+
+public interface itemclicklistner {
+
+    Void onClick(View view, int position , boolean isLongClick );
+}
---
+++ app/src/main/java/com/example/project/ebook/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.example.project.ebook.Prevalent;
+
+import com.example.project.ebook.Model.Users;
+
+public class Prevalent {
+    public static Users onlineuser;
+
+    public static final String phonekey = "Userphone";
+    public static final String passwordkey = "Userpassword";
+}
